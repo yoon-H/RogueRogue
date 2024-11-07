bcffafe9bb8b9812c9d152443d65364dccc8b182
yoon-H <ucando0320@gmail.com>
2024-11-07 12:20:55 +0900

    [ADD] add player.js

---
+++ js/player.js
@@ -0,0 +1,39 @@
+
+export class Player {
+    constructor() {
+        this._level = 1;
+        this._hp = 100;
+        this._minDamage = 5;
+        this._maxDamage = 10;
+        this._speed = 5;
+        this._equipment = {};       // 장비 칸
+        this._statusEffect = {};    // 방어구 칸
+    }
+
+    get level() {
+        return this._level;
+    }
+
+    get speed() {
+        return this._speed;
+    }
+
+    // 피격
+
+    get hp() {
+        return this._hp;
+    }
+
+    set hp(value) {
+        if (typeof value === 'Number') this._hp = value;
+    }
+
+    //공격력
+    get damage() {
+        //데미지 값 랜덤 계산
+        let amount = Math.floor(Math.random() * (this._maxDamage - this._minDamage + 1)) + this._minDamage;
+
+        return amount;
+    }
+
+}
