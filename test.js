aa104e95f59af176e1c2d3972cc83bd09f1831cc
yoon-H <ucando0320@gmail.com>
2024-11-08 19:46:34 +0900

    [UPDATE] package input system in test.js

--- test.js
+++ test.js
@@ -1,11 +1,23 @@
 import keypress from 'keypress';
 keypress(process.stdin);
 
-const options = ["Option 1", "Option 2", "Option 3", "Option 4"];
-let selectedIndex = 0;
+function selectOption() {
+    const options = ["Option 1", "Option 2", "Option 3", "Option 4"];
+    let index = 0;
 
-function renderOptions() {
+    return new Promise((resolve) => {
+        renderOptions(options, index);
+
+        select(options, index, resolve);  // resolve를 파라미터로 전달
+    });
+
+}
+
+
+
+function renderOptions(options, selectedIndex) {
     console.clear();
+    console.log(options);
     options.forEach((option, index) => {
         if (index === selectedIndex) {
             console.log(`> ${option}`); // 현재 선택된 항목에 화살표 표시
@@ -16,23 +28,35 @@ function renderOptions() {
     console.log("\nUse 1 (up), 2 (down), and Enter to select.");
 }
 
-// 키 입력 처리
-process.stdin.on("keypress", (ch, key) => {
-    console.log(key.name);
-    if (key && key.name === "up" || ch === '1') {
-        selectedIndex = (selectedIndex - 1 + options.length) % options.length;
-        renderOptions();
-    } else if (key && key.name === "down" || ch === '2') {
-        selectedIndex = (selectedIndex + 1) % options.length;
-        renderOptions();
-    } else if (key && key.name === "return") {
-        console.log(`\nYou selected: ${options[selectedIndex]}`);
-        process.exit();
-    } else if (key && key.ctrl && key.name === "c") {
-        process.exit();
-    }
-});
+function select(options, selectedIndex, resolve) {
+    process.stdin.setRawMode(true);
+    process.stdin.resume();
 
-process.stdin.setRawMode(true);
-process.stdin.resume();
-renderOptions();
+    // 키 입력 처리
+    process.stdin.on("keypress", (ch, key) => {
+        console.log(key.name);
+        if (key && key.name === "up" || ch === '1') {
+            selectedIndex = (selectedIndex - 1 + options.length) % options.length;
+            renderOptions(options, selectedIndex);
+        } else if (key && key.name === "down" || ch === '2') {
+            selectedIndex = (selectedIndex + 1) % options.length;
+            renderOptions(options, selectedIndex);
+        } else if (key && key.name === "return") {
+            console.log(`\nYou selected: ${options[selectedIndex]}`);
+            process.stdin.setRawMode(false);
+            process.stdin.pause();
+            resolve(selectedIndex);
+        } else if (key && key.ctrl && key.name === "c") {
+            process.exit();
+        }
+    });
+
+}
+
+async function play() {
+    let result = await selectOption();
+
+    console.log("result : " + result);
+}
+
+play();
