e93fa8749a6ad5b704172384001a41e3dbb666ad
yoon-H <ucando0320@gmail.com>
2024-11-15 17:58:57 +0900

    [UPDATE] add player stat by item

--- js/player.js
+++ js/player.js
@@ -9,7 +9,7 @@ export class Player {
         this._hp = this._MaxHP;
         this._minDamage = 2;
         this._maxDamage = 3;
-        this._inventory = {attack : 0, damage : 0, smoke : 0, heal : 0};
+        this._inventory = {attack : 0, hp : 0, smoke : 0, heal : 0};
         this._loc = new Point(0, 0);
     }
 
@@ -22,7 +22,7 @@ export class Player {
     }
 
     get MaxHp() {
-        return 40;
+        return this._MaxHP + 10 * Math.floor(this._inventory['hp'] /3); //3개 모으면 최대 체력 + 10
     }
 
     get hp() {
@@ -33,10 +33,23 @@ export class Player {
         if (typeof value === 'Number') this._hp = value;
     }
 
+    get inventory() {
+        return this._inventory;
+    }
+
+    get minDamage() {
+        return this._minDamage + 5 * Math.floor(this._inventory['attack'] /3); //3개 모으면 데미지 + 5
+    }
+
+    get maxDamage() {
+        return this.maxDamage + 5 * Math.floor(this._inventory['attack'] /3);
+    }
+
+
     // 데미지
     get damage() {
         //데미지 값 랜덤 계산
-        let amount = Tools.getRandomNum(this._minDamage, this._maxDamage);
+        let amount = Tools.getRandomNum(minDamage, maxDamage);
 
         return amount;
     }
